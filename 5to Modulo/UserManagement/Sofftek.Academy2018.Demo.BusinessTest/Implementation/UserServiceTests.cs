6356a64549e297b2abf690981ea835a686220a03
David Rodriguez Vazquez <david.rodriguezv@softtek.com>
2018-02-14 19:55:21 -0800

    data

---
+++ 5to Modulo/UserManagement/Sofftek.Academy2018.Demo.BusinessTest/Implementation/UserServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Softtek.Academy2018.Demo.Business.Contracts;
+using Softtek.Academy2018.Demo.Business.Implementation;
+using Softtek.Academy2018.Demo.Data.Contracts;
+using Softtek.Academy2018.Demo.Domain.Model;
+using System;
+
+namespace Sofftek.Academy2018.Demo.Business.Implementation.Test
+{
+    [TestClass()]
+    public class UserServiceTests
+    {
+        [TestMethod()]
+        public void When_add_a_valid_user()
+        {
+            int expectedId = 1;
+            Mock<IUserRepository> repository = new Mock<IUserRepository>();
+            repository.Setup(x => x.Exist(It.IsAny<string>())).Returns(true);
+            IUserService service = new UserService(repository.Object);
+
+            User user = new User
+            {
+                IS = "JAMU",
+                FirstName = "Juan",
+                LastName = "Ramos",
+                DateOfBirth = new DateTime(1984, 12, 12)
+            };
+
+            int resultId = service.Add(new User());
+            Assert.AreEqual(expectedId, resultId);
+        }
+    }
+}
---
+++ 5to Modulo/UserManagement/Softtek.Academy.2018.Demo.Domain/Enum/EnumLevel.cs
@@ -0,0 +1,12 @@
+﻿namespace Softtek.Academy2018.Demo.Domain.Enum
+{
+    public enum Level
+    {
+        DM = 1, //Many projects
+        PM = 2, //two projects
+        TL = 3, //one project
+        Developer = 4, //one project
+        QA = 5, //five projects
+        Momentum = 6 //zero project
+    }
+}
