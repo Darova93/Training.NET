0a1f073982bbc2a7b9b3e7c89ab34e0aa1fe54d3
David Rodriguez Vazquez <david.rodriguezv@softtek.com>
2018-03-08 21:01:07 -0800

    work

--- 7mo Modulo/Practica 5 Users/user-list/user-list.component.js
+++ 7mo Modulo/Practica 5 Users/user-list/user-list.component.js
@@ -2,7 +2,7 @@
 var app = angular.module('AcademyApp', [])
 app.controller('UserController', function UserController($http) {
 
-    this.users = [{
+    this.user = [{
         is: "DRVA",
         fname: "David",
         last_name: "Rodriguez",
@@ -13,12 +13,12 @@ app.controller('UserController', function UserController($http) {
 
     this.SendUser = function () {
         var data = {
-            is: "DRVA",
-            first_name: "David",
-            last_name: "Rodriguez",
-            second_last_name: "Vazquez",
-            hour: "10-10-2018T17:00:00",
-            calification: "10"
+            is: this.userIs,
+            first_name: this.userFname,
+            last_name: this.userLname,
+            second_last_name: this.userSlname,
+            hour: this.userTime,
+            calification: this.userGrade
         };
 
         var jsondata = JSON.stringify(data);
@@ -37,13 +37,13 @@ app.controller('UserController', function UserController($http) {
 
     this.UpdateUser = function(){
         var data = {
-            first_name: "Deivid",
-            last_name: "rodrigus",
-            second_last_name: "vasqus"
+            first_name: this.userFname,
+            last_name: this.userLname,
+            second_last_name: this.userSlname
         };
 
         var jsondata = JSON.stringify(data);
-        var baseUrl = 'http://10.0.16.28:3000/users/4';
+        var baseUrl = 'http://10.0.16.28:3000/users/'+this.updateID;
 
         var config = {
             method: "PUT",
@@ -54,11 +54,12 @@ app.controller('UserController', function UserController($http) {
             }
         };
         return $http(config);
+        this.updateID=""
     }
 
     this.DeleteUser = function(){
 
-        var baseUrl = 'http://10.0.16.28:3000/users/5';
+        var baseUrl = 'http://10.0.16.28:3000/users/'+this.deleteID;
 
         var config = {
             method: "DELETE",
@@ -68,6 +69,27 @@ app.controller('UserController', function UserController($http) {
             }
         };
         return $http(config);
+        this.deleteID="";
     }
 
+    // this.searchUser = function(){
+
+    //     var baseUrl = 'http://10.0.16.28:3000/users/'+this.searchID;
+
+    //     var config = {
+    //         method: "GET",
+    //         url: baseUrl,
+    //         headers: {
+    //             'Content-Type': 'application/json; charset=utf-8'
+    //         }
+    //     };
+    //     this.searchedUser = $http(config).response.data;
+    //     this.searchID="";
+    // }
+});
+
+app.controller('UserController', function UserController($scope, $http) {
+    $http.get("http://10.0.16.28:3000/users/").then(function (response) {
+        $scope.myData = response.data;
+    });
 });
